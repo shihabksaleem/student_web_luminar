fa1b1285a338f085e695803204e89f867a89f413
MeAnandCB <Meanandcb98@gmail.com>
2024-02-24 08:44:13 +0530

    login updated

--- lib/presentation/mobile/batch_bottom_tab/view/widgets/batch_container.dart
+++ lib/presentation/mobile/batch_bottom_tab/view/widgets/batch_container.dart
@@ -156,112 +156,104 @@ class _BatchContainerState extends State<BatchContainer> {
               physics: const NeverScrollableScrollPhysics(),
               itemCount: batchscreenprovider.batchList.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) => Container(
-                child: Column(
-                  children: [
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            offset: const Offset(0, 8),
-                            color: ColorConstant.lightGrey.withOpacity(.20),
-                            blurRadius: 6,
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color(0xffEDE9EE),
-                      ),
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const SizedBox(),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.dvr,
-                                  color: ColorConstant.primary1,
-                                  size: 30,
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    const SizedBox(),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          batchscreenprovider
-                                                  .batchList[index].batchName ??
-                                              '',
-                                          style: const TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w600),
-                                        ),
-                                        const SizedBox(height: 5),
-                                        Text(
-                                          batchscreenprovider
-                                                  .batchList[index].startdate ??
-                                              "",
-                                          style: const TextStyle(
-                                            fontSize: 14,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox()
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  "Luminar Tecnolab",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                Text(
-                                  batchscreenprovider.batchList[index].course ==
-                                          true
-                                      ? "Online"
-                                      : "Offline",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: batchscreenprovider
-                                                  .batchList[index].course ==
-                                              false
-                                          ? Colors.red
-                                          : Colors.green),
-                                ),
-                              ],
-                            ),
-                            const SizedBox()
-                          ],
-                        ),
-                      ),
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  mainAxisExtent: 200),
+              itemBuilder: (context, index) => InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => BatchDetails(
+                      batchname:
+                          batchscreenprovider.batchList[index].batchName ?? "",
+                      batchId:
+                          batchscreenprovider.batchList[index].id.toString(),
                     ),
-                    const SizedBox(
-                      height: 20,
-                    )
-                  ],
+                  ));
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: const Color(0xffEDE9EE),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 8),
+                        color: ColorConstant.lightGrey.withOpacity(.20),
+                        blurRadius: 6,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.dvr,
+                            color: ColorConstant.primary1,
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SizedBox(),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    batchscreenprovider
+                                            .batchList[index].batchName ??
+                                        '',
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    batchscreenprovider
+                                            .batchList[index].startdate ??
+                                        "",
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Luminar Tecnolab",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            batchscreenprovider.batchList[index].course == true
+                                ? "Online"
+                                : "Offline",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: batchscreenprovider
+                                            .batchList[index].course ==
+                                        false
+                                    ? Colors.red
+                                    : Colors.green),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
